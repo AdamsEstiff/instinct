c8b471b67a2ed4f47dc1cd86ab11531dff5491f7
tfreddy <tfreddy2013@outlook.com>
2019-02-24 19:10:05 -0600

    2nd Commit FT

--- resources/views/user/Publication.blade.php
+++ resources/views/user/Publication.blade.php
@@ -2,57 +2,91 @@
 
 
 @section('content')
-    <div class="content">
-        <div class="row justify-content-center">
-            <div class=" text-center">
-                <div class="col-md-12">
+    <div class="row justify-content-center">
+        <div class=" row">
+            <div class="col-sm-13">
 
-                    <div class="card">
-                        <form class="card-header" action="add" method="POST" enctype="multipart/form-data" role="form">
-                            <div class="input-group">
-                                <input name="user_id" type="hidden" value="{{Auth::User()->id}}">
+                <div class="card">
+
+                    <form class="card-header" action="add" method="POST" enctype="multipart/form-data" role="form">
+                        <div class="row">
+                            <div class="col-sm-6">
+
+                                    <label class="card">
+                                        <input type="file" name="image" class="custom-file-input btn"
+                                               value="{{asset("images/yope.jpg")}}" id="validatedCustomFile "
+                                               required>
+                                        <img class="card-img-top" src="{{asset("images/agregar.png")}}"
+                                             alt="Card image cap">
+                                    </label>
+
+                                <div class="input-group">
+                                    <input name="user_id" type="hidden" value="{{Auth::User()->id}}">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="container-fluid">
+                                        </br>
+                                        {{csrf_field() }}
+
+                                        <div class="container-fluid">
+                                            <h3 for="formGroupExampleInput">Datos Iniciales</h3>
+                                            <label for="inputEmail4">Nombre</label>
+                                            <input type="text" name="nombre_p" placeholder="Nombre" class="form-control">
+                                            </br>
+                                            <div class="row">
+                                                <div class="col">
+                                                    <label for="inputEmail4">Cantidad</label>
+                                                    <input class="form-control" type="number" name="cantidad"
+                                                           placeholder="Cantidad">
+                                                </div>
+                                                <div class="col">
+                                                    <label for="inputEmail4">Precio</label>
+                                                    <input class="form-control" type="number" name="precio" placeholder="Precio">
+                                                </div>
+                                            </div>
+                                            </br>
+                                            <label for="inputEmail4">Contacto</label>
+                                            <input class="form-control" type="text" name="contacto" placeholder="Contacto">
+                                            <label for="inputEmail4">Dirección</label>
+                                            <input class="form-control" type="text" name="dirrecion" placeholder="Direccion">
+                                            <label for="inputEmail4">Comentario</label>
+                                            <textarea class="form-control" type="text" name="comment"
+                                                      placeholder="Comentario"></textarea>
+                                            <label for="inputEmail4">Descripción</label>
+                                            <textarea class="form-control" name="descripcion" placeholder="Descripcion..."
+                                                      class="form-control"
+                                                      id="exampleFormControlTextarea1" rows="2"></textarea>
+                                            </br>
 
 
+                                        </div>
+                                    </div>
+                                </div>
 
                             </div>
-                            <h3 for="formGroupExampleInput">Datos Iniciales</h3>
-                            <label for="inputEmail4">Nombre</label>
-                            <input type="text" name="nombre_p" placeholder="Nombre" class="form-control">
-                            <label for="inputEmail4">Cantidad</label>
-                            <input class="form-control" type="number" name="cantidad" placeholder="Cantidad">
-                            <label for="inputEmail4">Precio</label>
-                            <input class="form-control" type="number" name="precio" placeholder="Precio">
-                            <label for="inputEmail4">Contacto</label>
-                            <input class="form-control" type="text" name="contacto" placeholder="Contacto">
-                            <label for="inputEmail4">Dirección</label>
-                            <input class="form-control" type="text" name="dirrecion" placeholder="Direccion">
-                            <label for="inputEmail4">Comentario</label>
-                            <textarea class="form-control" type="text" name="comment" placeholder="Comentario"></textarea>
-                            <label for="inputEmail4">Descripción</label>
-                            <textarea class="form-control" name="descripcion" placeholder="Descripcion..." class="form-control"
-                                      id="exampleFormControlTextarea1" rows="2"></textarea>
-                            <div class="input-group-append" id="button-addon4">
-                                <button class="btn btn-primary" type="submit">Publicar</button>
-                                <a href="{{asset('home')}}" class="btn btn-danger">Cancelar</a>
-                            </div>
 
-                            <div class="card-body">
-                                </br>
-                                {{csrf_field() }}
-                                <label class="card">
-                                    <input type="file" name="image" class="custom-file-input btn"
-                                           value="{{asset("images/yope.jpg")}}" id="validatedCustomFile " required>
-                                    <img class="card-img-top" src="{{asset("images/agregar.png")}}"
-                                         alt="Card image cap">
-                                </label>
-                            </div>
-                        </form>
-                    </div>
+
+
+
+
+                        </div>
+                        <div class="container" id="button-addon4">
+                            <button class="btn btn-outline-primary" type="submit">Publicar</button>
+
+                            <a href="{{asset('home')}}" class="btn btn-outline-danger">Cancelar</a>
+                        </div>
+                    </form>
+
                 </div>
+
             </div>
+
         </div>
 
     </div>
+
     @include('user.search2')
 @endsection
 @section('script')
